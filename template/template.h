2dd3d6a9b9fa537ccf7d1d1b572c8acb5f88a522
Soulou <leo.unbek@gmail.com>
2012-03-07 00:19:14 +0100

    Ajout template pour header C

---
+++ template/template.h
@@ -0,0 +1,21 @@
+/*
+ * @File : ${filename}.h
+ *
+ * @Author : Unbekandt Leo <leo@soulou.fr>
+ *
+ * @Summary :  ${cursor}
+ *
+ * @Version : ${date}
+ *
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <leo@soulou.fr> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return 
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef _${FILENAME}_H
+#define _${FILENAME}_H
+
+#endif
