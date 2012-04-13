745a6daa52fdb3aecc46d4f160a2b7b638e0f05c
Soulou <leo.unbek@gmail.com>
2012-04-13 13:25:11 +0300

    Clone quiet

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ -x "/usr/bin/git" ] ; then
-  git clone 'https://github.com/ares-ensiie/vim_conf' "$HOME/vim_conf"
+  git clone -q 'https://github.com/ares-ensiie/vim_conf' "$HOME/vim_conf"
   cd "$HOME/vim_conf"
-  git submodule update --init
+  git submodule -q update --init
   if [ -d "$HOME/.vim" ] ; then
     mv "$HOME/.vim" "$HOME/.vim.bak"
   fi
