af6e59d55b0d12819e2d134af91d6828ed7fbdde
Soulou <leo.unbek@gmail.com>
2012-04-13 10:21:30 +0300

    Change reference to repo

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ -x "/usr/bin/git" ] ; then
-  git clone 'https://github.com/Soulou/Soulou-s-Vim' "$HOME/Soulou-s-Vim"
+  git clone 'https://github.com/ares-ensiie/vim_conf' "$HOME/vim_conf"
   if [ -d "$HOME/.vim" ] ; then
     mv "$HOME/.vim" "$HOME/.vim.bak"
   fi
-  mv "$HOME/Soulou-s-Vim" "$HOME/.vim"
+  mv "$HOME/vim_conf" "$HOME/.vim"
 else
   echo "Please install git" 1>&2
   exit -1
